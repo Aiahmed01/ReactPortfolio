99faf5c9108491a11f27f643dcd763f0cfaf0cfc
Abdulla <default@defaults-iMac.local>
2023-08-09 11:59:22 -0500

    submit3

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -25,7 +25,7 @@ export default function Card(props) {
        I'm {props.name}. 🤓</p>
       <p>I'm a Software Engineer AKA Full Stack application developer. </p>
       <p>Passionate about creating beautiful and functional websites.</p>
-      <p>Click the MERN Image to download my resume 🏔 ⛰ 🌋</p>
+      <p>Click the MERN Image to view google doc of my resume 🏔 ⛰ 🌋</p>
     <a href='https://docs.google.com/document/d/1FXEzf187iVNuXznW-Ndlb_iMA7eG9CQG/edit?usp=sharing&ouid=111223800472406090912&rtpof=true&sd=true' target="_blank"> <img src={mern}  onClick = {handleImgClick} className="mern-image btn btn-primary"/> </a>
    
   </div>
